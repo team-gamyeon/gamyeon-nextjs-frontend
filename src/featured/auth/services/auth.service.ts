b15e7f4eddc95a599f966e00e733b16c777a84ca
christina-ahn <ysahn@tlksys.com>
2026-08-04 16:40:29 +0900

    chore: 엔드포인트 수정

--- src/featured/auth/services/auth.service.ts
+++ src/featured/auth/services/auth.service.ts
@@ -14,5 +14,5 @@ export async function getCurrentUser(): Promise<User> {
 }
 
 export async function withdrawUser(): Promise<void> {
-  await serverApi.delete('/api/v1/auth/me')
+  await serverApi.delete('/api/v1/users/me')
 }
